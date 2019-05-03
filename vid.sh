41469abbe795dfcf8b8f3980ac4353211a6624d6
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-05-03 10:12:45 +0700

    Add files via upload

---
+++ vid.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+#buat sendiri goblok
+#jangan recode tools orang asw
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 2
+cat blue.txt | lolcat
+sleep 3
+echo
+echo
+figlet "GUA GANS" | lolcat
+echo ""
+echo "\033[32;1m[$]\033[34;1m====================>\033[35;1m[$]"
+echo "\033[34;1mAuthor : Mr.51MB4H"
+echo "\033[35;1mCONTACT:082216XXXXXX"
+echo "\033[31;1mPLAYER : SOLO PLAYER!!!"
+echo "\033[34;1m[$]\033[36;1m====================>\033[35;1m[$]"
+echo ""
+sleep 2
+echo "\033[36;1mSilahkan Pilih Videonya"
+echo "\033[35;1m"
+sleep 4
+echo "[-------------------------------"
+sleep 3
+echo $green "1]>VIDEO HACKER"
+echo "[--------------------------------------]>"
+sleep 3
+echo $blue "2]>VIDEO SERAM"
+echo "[--------------------------------------]>"
+sleep 3
+echo $green "3]VIDEO NGAGETIN"
+echo "[--------------------------------------]>"
+sleep 3
+echo $blue "4]VIDEO 18+"
+echo "[--------------------------------------]>-"
+sleep 3
+echo $green "5]EXIT"
+echo "[--------------------------------------]>"
+echo $yellow "============="
+read -p      "pillh no nya╙>>>>]>" see
+date | lolcat
+
+sleep 2
+if [ $see  = 1 ]|| [ $see = 01 ]
+then
+clear
+figlet "MR.51MB4H" | lolcat
+termux-setup-storage
+sleep 5
+mv HACK.zip $HOME /sdcard
+echo $purple "SABAR YA CUK!!"
+sleep 7
+echo $green "masih load ANJING!!"
+sleep 5
+echo $red "HAMPIR SELESAI"
+sleep 4
+echo $yellow"JADIKAN MOTIVASI CUK!!"
+sleep 3
+echo $blue "SELESAI CUK,NONTONNTA JANGAN NGILER ASU!!!"
+sleep 2
+echo $purple"HAHAHAHAHA"
+echo
+echo $cyan"CARI DIFOLDER HACK.ZIP LALU LU UNZIP DULU"
+fi
+
+
+sleep 2
+if [ $see  = 2 ]|| [ $see = 02 ]
+then
+clear
+toilet "SERAM" | lolcat
+termux-setup-storage
+sleep 5
+mv  Takut.zip $HOME /sdcard
+echo $purple "SABAR YA CUK!!"
+sleep 7
+echo $green "masih load ANJING!!"
+sleep 5
+echo $red "HAMPIR SELESAI"
+sleep 4
+echo $yellow" JANGAN TAKUT  CUK!!"
+sleep 3
+echo $blue "SELESAI CUK SIAPIN AMBULAN"
+sleep 2
+echo $purple"HAHAHAHAHA"
+echo
+echo $cyan"CARI DIFOLDER TAKUT.ZIP LALU LU UNZIP DULU ASU!!"
+fi
+
+sleep 2
+if [ $see  = 3 ]|| [ $see = 03 ]
+then
+clear
+figlet  "KAGET" | lolcat
+termux-setup-storage
+sleep 5
+mv KAGET.zip $HOME/sdcard
+echo $purple "SABAR YA CUK!!"
+sleep 7
+echo $green "masih load ANJING!!"
+sleep 5
+echo $red "HAMPIR SELESAI"
+sleep 4
+echo $yellow"JANGANKAGET LU NJING!!"
+sleep 3
+echo $blue "BUAT STORY SW / SHARE KE GRUP COCOK CUK!!"
+sleep 2
+echo $purple"HAHAHAHAHA"
+echo
+echo $cyan"CARI DIFOLDER KAGET.ZIP LALU LU UNZIP DULU"
+fi
+
+sleep 2
+if [ $see  = 4 ]|| [ $see = 04 ]
+then
+clear
+toilet  "XNXX" | lolcat
+sleep 5
+echo $purple "COMING SOON!!"
+echo $green "HAHA KETIPU LU"
+echo $blue "tool ini masih dalam proses"
+fi
+
+
+if [ $see  = 5 ]|| [ $see = 05 ]
+then
+clear
+figlet  "minggat sono" | lolcat
+sleep 5
+echo $green "THANKS DAH GUNAKAN TOOL INI!!"
+sleep 5
+echo $red "SEMOGA KAU CEPAT MATI YA....."
+sleep 4
+echo $yellow"EH SALAH CUK SEMOGA LU SUKA..!!"
+sleep 3
+echo $blue "GW HANYA SEORANG NOOB!!"
+sleep 2
+echo $purple"YANG INGIN TERUS BERKARYA"
+echo
+echo $cyan"BYE CUK!!!!!"
+figlet "SEE YOU NJING" | lolcat
+fi
